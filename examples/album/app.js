2e2acb1f6e2f85782aeb65db69c63b10ac2a6447
carlosmintfan <118076740+carlosmintfan@users.noreply.github.com>
2025-01-14 14:37:48 -0300

    Use cat photos for album example
    Cats are more neutral. I don't like what there was before.

--- examples/album/app.js
+++ examples/album/app.js
@@ -1,4 +1,4 @@
-var perPage = 6
+var perPage = 3
   , prev = document.querySelector('#prev')
   , next = document.querySelector('#next');
 
@@ -45,47 +45,12 @@ function adjustPager(page) {
 }
 
 var images = [
-    'http://upload.wikimedia.org/wikipedia/en/7/76/Grim_Fandango_artwork.jpg'
-  , 'http://www.xblafans.com/wp-content/uploads//2011/08/Grim-Fandango1.jpg'
-  , 'http://media.giantbomb.com/uploads/0/1371/190604-grimfandango106_super.jpg'
-  , 'http://gamejunkienz.files.wordpress.com/2012/02/grimfandango.jpg'
-  , 'http://onlyhdwallpapers.com/wallpaper/video_games_grim_fandango_lucas_arts_desktop_1024x768_wallpaper-305343.jpg'
-  , 'http://lparchive.org/Grim-Fandango-(Screenshot)/Update%207/02176.gif'
-  , 'http://bulk2.destructoid.com/ul/128679-GrimFandangoActionFigures.jpg'
-  , 'http://www.gamasutra.com/features/20061103/grimfandango02.jpg'
-  , 'http://metavideogame.files.wordpress.com/2011/05/grimhof_03_1081459316.jpg'
-  , 'http://3.bp.blogspot.com/_zBnIHQUy4r4/SpxdDw1Z8tI/AAAAAAAABJM/FoCWfc8imnc/s400/GrimFandango1024x768.jpg'
-  , 'http://www.deviantart.com/download/184571597/grim_fandango_by_domigorgon-d31w0ct.jpg'
-  , 'http://vgboxart.com/boxes/PC/29535-grim-fandango.png?t=1243105773'
-  , 'http://kastatic.com/i2/games/1/3/13230/10.png'
-  , 'http://www.thunderboltgames.com/s/img600/grimfandango.jpg'
-  , 'http://i2.listal.com/image/1425291/936full-grim-fandango-artwork.jpg'
-  , 'http://www.xblafans.com/wp-content/uploads//2011/08/Grim-Fandango1.jpg'
-  , 'http://media.giantbomb.com/uploads/0/1371/190604-grimfandango106_super.jpg'
-  , 'http://gamejunkienz.files.wordpress.com/2012/02/grimfandango.jpg'
-  , 'http://onlyhdwallpapers.com/wallpaper/video_games_grim_fandango_lucas_arts_desktop_1024x768_wallpaper-305343.jpg'
-  , 'http://lparchive.org/Grim-Fandango-(Screenshot)/Update%207/02176.gif'
-  , 'http://bulk2.destructoid.com/ul/128679-GrimFandangoActionFigures.jpg'
-  , 'http://www.gamasutra.com/features/20061103/grimfandango02.jpg'
-  , 'http://metavideogame.files.wordpress.com/2011/05/grimhof_03_1081459316.jpg'
-  , 'http://3.bp.blogspot.com/_zBnIHQUy4r4/SpxdDw1Z8tI/AAAAAAAABJM/FoCWfc8imnc/s400/GrimFandango1024x768.jpg'
-  , 'http://www.deviantart.com/download/184571597/grim_fandango_by_domigorgon-d31w0ct.jpg'
-  , 'http://vgboxart.com/boxes/PC/29535-grim-fandango.png?t=1243105773'
-  , 'http://kastatic.com/i2/games/1/3/13230/10.png'
-  , 'http://www.thunderboltgames.com/s/img600/grimfandango.jpg'
-  , 'http://i2.listal.com/image/1425291/936full-grim-fandango-artwork.jpg'
-  , 'http://www.xblafans.com/wp-content/uploads//2011/08/Grim-Fandango1.jpg'
-  , 'http://media.giantbomb.com/uploads/0/1371/190604-grimfandango106_super.jpg'
-  , 'http://gamejunkienz.files.wordpress.com/2012/02/grimfandango.jpg'
-  , 'http://onlyhdwallpapers.com/wallpaper/video_games_grim_fandango_lucas_arts_desktop_1024x768_wallpaper-305343.jpg'
-  , 'http://lparchive.org/Grim-Fandango-(Screenshot)/Update%207/02176.gif'
-  , 'http://bulk2.destructoid.com/ul/128679-GrimFandangoActionFigures.jpg'
-  , 'http://www.gamasutra.com/features/20061103/grimfandango02.jpg'
-  , 'http://metavideogame.files.wordpress.com/2011/05/grimhof_03_1081459316.jpg'
-  , 'http://3.bp.blogspot.com/_zBnIHQUy4r4/SpxdDw1Z8tI/AAAAAAAABJM/FoCWfc8imnc/s400/GrimFandango1024x768.jpg'
-  , 'http://www.deviantart.com/download/184571597/grim_fandango_by_domigorgon-d31w0ct.jpg'
-  , 'http://vgboxart.com/boxes/PC/29535-grim-fandango.png?t=1243105773'
-  , 'http://kastatic.com/i2/games/1/3/13230/10.png'
-  , 'http://www.thunderboltgames.com/s/img600/grimfandango.jpg'
-  , 'http://i2.listal.com/image/1425291/936full-grim-fandango-artwork.jpg'
+  "https://images.unsplash.com/photo-1571566882372-1598d88abd90",
+  "https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba",
+  "https://images.unsplash.com/photo-1506755855567-92ff770e8d00",
+  "https://images.unsplash.com/photo-1495360010541-f48722b34f7d",
+  "https://images.unsplash.com/photo-1545249390-6bdfa286032f",
+  "https://images.unsplash.com/photo-1504384558400-c1347a7bd18f",
+  "https://images.unsplash.com/photo-1549545931-59bf067af9ab",
+  "https://images.unsplash.com/photo-1479065476818-424362c3a854"
 ];
